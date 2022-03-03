29267ee85d04fea3af06dd2f00764c41077183b7
BaurRitto <baurzhan87@gmail.com>
2022-03-03 08:19:54 -0500

    changed name for save path

--- cron_job.sh
+++ cron_job.sh
@@ -5,6 +5,7 @@ python generate_random_message.py
 #put to hdfs
 hdfs dfs -put -f message_test.txt /user/baur/output/
 
+nice_date='+%Y%m%d_%H%M%S'
 #run yarn job
-yarn jar target/lab1-1.0-SNAPSHOT-jar-with-dependencies.jar /user/baur/LICENSE.txt "/output_$(date + %Y%m%d_%H%M%S)"
+echo "$(yarn jar target/lab1-1.0-SNAPSHOT-jar-with-dependencies.jar /user/baur/LICENSE.txt /output_$(date "$nice_date"))"
 
